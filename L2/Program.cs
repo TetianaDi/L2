9f3b6c1d8a9081280131fce5d5fabf7fe7ff36e2
INFOPULSE\Tetiana.Dibrova <te.dibrova@gmail.com>
2023-03-08 22:11:22 +0200

    Initial commit

---
+++ L2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace L2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var firstVariable = Console.ReadLine();
+
+            int intVarible;
+            int intVarible1, intVarible2, intVarible3;
+
+            int intVarible_;
+
+            int intVaribleWithValue = 5;
+            var varVaribleWithValue = "tt";
+            char variable = 'd';
+
+            Console.WriteLine($"intVaribleWithValue={intVaribleWithValue}");
+            Console.WriteLine("varVaribleWithValue={0}", varVaribleWithValue);
+            Console.WriteLine("varible" + variable + "  ");
+
+
+        }
+    }
+}
